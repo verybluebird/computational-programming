bd9d1fa7e81faf4c112e09ed37df97aa4c73587c
Juliya Zatolockaya <zatolockaya14@gmail.com>
2021-03-24 13:57:39 +0700

    modified

--- 2.for
+++ 2.for
@@ -1,29 +1,42 @@
       implicit none
+      integer m,n
       real minx, maxx, deltax, miny, maxy, deltay
       common minx, maxx, deltax, miny, maxy, deltay
-      
-      
+      common m,n
+      call input()
+      call calc_m_n()
+      end
       
       
       subroutine input()
        implicit none
        real minx, maxx, deltax, miny, maxy, deltay
        common minx, maxx, deltax, miny, maxy, deltay
-       open (1,FILE=’input.txt’)
-       read (1,FILE=’input.txt’) minx, maxx, deltax, 
-    & miny, maxy, deltay
+       open (1,FILE='input.txt')
+       read(1,*)minx,maxx,deltax,miny,maxy,deltay
        close (1)
       end
       
       subroutine calc_m_n()
+       integer m,n
        real minx, maxx, deltax, miny, maxy, deltay
        common minx, maxx, deltax, miny, maxy, deltay
        common m,n
+       m = (maxy-miny)/deltay
+       if ((maxy - miny - m*deltay).GE. 0) 
+       then m = m + 1
+       end if
+       
+       n = (maxx-minx)/deltax
+       if ((maxx - minx - n*deltax).GE. 0) 
+       then n = n + 1
+       end if
+       
       end
       
       
       subroutine out()
-        write (1,FILE=’output.txt’)table
+        write (1,FILE='output.txt')table
       end
       
       logical function input_chek()
@@ -47,5 +60,5 @@
        common minx, maxx, deltax, miny, maxy, deltay
        
        OPEN(2, FILE = 'output.txt', STATUS = 'NEW', ERR = 1)
-       
+      end
        
